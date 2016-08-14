f2ce1f01d8a3af1a001c7a1ce1ce898fe4ac2c17
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-08-14 20:49:23 +0100

    Turning off printout

--- ci_tester.sh
+++ ci_tester.sh
@@ -1,6 +1,6 @@
 # Test environment
 jupyter nbconvert --to markdown --execute --ExecutePreprocessor.timeout=120 content/infrastructure/check_gds_stack.ipynb
 # Run all notebooks, allow to fail, print out
-jupyter nbconvert --execute --allow-errors --ExecutePreprocessor.timeout=120 --stdout content/labs/*.ipynb
+#jupyter nbconvert --to markdown --execute --allow-errors --ExecutePreprocessor.timeout=120 --stdout content/labs/*.ipynb
 # Run all notebooks, do not allow failures, do not print out
 jupyter nbconvert --execute --ExecutePreprocessor.timeout=120 content/labs/*.ipynb
